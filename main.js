837ffcd5107819c6814d460a84a8ba6f30b605e7
agumichaeleze <agumichael72@gmail.com>
2023-05-24 07:23:58 +0100

    project1.js question 1-5

---
+++ main.js
@@ -0,0 +1,62 @@
+//1. Print numbers from 1 to 10
+for(let number = 0; number <=10; number++){
+    console.log(number)
+}
+console.log('---------------------------------')
+
+//2. Print the odd numbers less than 100
+for(let odd = 1; odd < 100; odd+=2){
+console.log(odd)
+}
+console.log('---------------------------------')
+
+//3. Print the multiplication table with 7
+for(let multiple = 7; multiple<=84; multiple+=7){
+    console.log(multiple)
+}
+console.log('---------------------------------')
+//4. Print all the multiplication tables with numbers from 1 to 10
+for(let two = 2; two<=24; two+=2){
+    console.log(two)
+}
+for(let three = 3; three<=36; three+=3){
+    console.log(three)
+}
+for(let four = 4; four<=48; four+=4){
+    console.log(four)
+}
+for(let five = 5; five<=60; five+=5){
+    console.log(five)
+}
+for(let six = 6; six<=72; six+=6){
+    console.log(six)
+}
+for(let seven = 7; seven<=84; seven+=7){
+    console.log(seven)
+}
+for(let eight = 8; eight<=96; eight+=7){
+    console.log(eight)
+}
+for(let nine = 9; nine<=108; nine+=9){
+    console.log(nine)
+}
+for(let ten = 10; ten<=120; ten+=10){
+    console.log(ten)
+}
+for(let eleven = 11; eleven<=132; eleven+=11){
+    console.log(eleven)
+}
+for(let twelve = 12; twelve<=144; twelve+=12){
+    console.log(twelve)
+}
+console.log('---------------------------------')
+//5. Calculate the sum of numbers from 1 to 10
+// let sumNum =  2+4+6+8+10
+// console.log(sumNum)
+function sumNum(){
+    console.log(2+4+6+8+10)
+}
+sumNum()
+console.log('---------------------------------')
+
+
